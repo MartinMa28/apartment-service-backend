520d12426483c6cc0784fb55986139ae03052754
Yilin Ma <1135485226@qq.com>
2020-12-08 02:29:31 -0800

    change url

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/user', usersRouter);
 app.use('/auth', authRouter);
 
 app.get('/*', (req, res) => {
