ae3528ac27278d5ce880ec9a8e6272916764c6fd
Yilin Ma <1135485226@qq.com>
2020-12-09 20:42:42 -0800

    Update query endpoint

--- routes/houses.js
+++ routes/houses.js
@@ -2,40 +2,7 @@ import express from 'express';
 
 const router = express.Router();
 
-router.get('/simp/:page', async (req, res) => {
-  try {
-    const { rooms, minPrice, maxPrice } = req.query;
-    const { page } = req.params;
-    const pageSize = 24;
-    const query = {
-      bedrooms: parseInt(rooms),
-      'result-price': { $gt: parseInt(minPrice), $lt: parseInt(maxPrice) },
-    };
-
-    const cursor = req.app.locals.db
-      .collection('apartment')
-      .find(query)
-      .sort({ _id: 1 })
-      .skip((parseInt(page) - 1) * pageSize)
-      .limit(pageSize);
-
-    const apartments = await cursor.toArray();
-
-    const countCursor = req.app.locals.db.collection('apartment').find(query);
-    const apartmentCount = await countCursor.count();
-
-    res.status(200).json({
-      count: apartmentCount,
-      apartments: apartments,
-    });
-  } catch (err) {
-    res.status(500).json({
-      message: `Internal error: ${err}`,
-    });
-  }
-});
-
-router.get('/comp/:page', async (req, res) => {
+router.get('/', async (req, res) => {
   try {
     const {
       rooms,
@@ -45,15 +12,26 @@ router.get('/comp/:page', async (req, res) => {
       maxArea,
       minDate,
       maxDate,
+      page,
     } = req.query;
-    const { page } = req.params;
+
     const pageSize = 24;
-    const query = {
-      bedrooms: parseInt(rooms),
-      'result-price': { $gt: parseInt(minPrice), $lt: parseInt(maxPrice) },
-      area: { $gt: parseInt(minArea), $lt: parseInt(maxArea) },
-      'result-date': { $gt: new Date(minDate), $lt: new Date(maxDate) },
-    };
+    let query;
+
+    if (minArea && maxArea && minDate && maxDate) {
+      query = {
+        bedrooms: parseInt(rooms),
+        'result-price': { $gt: parseInt(minPrice), $lt: parseInt(maxPrice) },
+        area: { $gt: parseInt(minArea), $lt: parseInt(maxArea) },
+        'result-date': { $gt: new Date(minDate), $lt: new Date(maxDate) },
+      };
+    } else {
+      query = {
+        bedrooms: parseInt(rooms),
+        'result-price': { $gt: parseInt(minPrice), $lt: parseInt(maxPrice) },
+      };
+    }
+
     const cursor = req.app.locals.db
       .collection('apartment')
       .find(query)
@@ -68,6 +46,7 @@ router.get('/comp/:page', async (req, res) => {
 
     res.status(200).json({
       count: apartmentCount,
+      pages: Math.ceil(apartmentCount / pageSize),
       apartments: apartments,
     });
   } catch (err) {
